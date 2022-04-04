04eb05064e551a350c368e78f0c45fa638ed3317
Antonio Martín <ajms66@gmail.com>
2022-04-04 18:06:13 +0200

    proyecto 4

---
+++ 04_crud_alumnos_rest/src/main/java/com/sanitas/service/AlumnosService.java
@@ -0,0 +1,13 @@
+package com.sanitas.service;
+
+import java.util.List;
+
+import com.sanitas.dtos.AlumnoDto;
+
+public interface AlumnosService {
+	boolean altaAlumno(AlumnoDto alumno);
+	void eliminarAlumno(String nombre);
+	List<AlumnoDto> alumnosCurso(String curso);
+	List<AlumnoDto> todosAlumnos();
+	void actualizarAlumno(AlumnoDto alumno);
+}
---
+++ 04_crud_alumnos_rest/src/main/java/com/sanitas/service/AlumnosServiceImpl.java
@@ -0,0 +1,57 @@
+package com.sanitas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sanitas.converters.AlumnoConverter;
+import com.sanitas.dao.AlumnosDao;
+import com.sanitas.dtos.AlumnoDto;
+
+@Service
+public class AlumnosServiceImpl implements AlumnosService {	
+	@Autowired
+	AlumnoConverter alumnoConverter;
+	@Autowired
+	AlumnosDao alumnosDao;
+	@Override
+	public boolean altaAlumno(AlumnoDto alumno) {
+		if(alumnosDao.findByNombre(alumno.getNombre()).isEmpty()) {
+			alumnosDao.save(alumnoConverter.dtoToAlumno(alumno));
+			return true;
+		}
+		return false;
+	}
+
+	
+
+	@Override
+	public List<AlumnoDto> alumnosCurso(String curso) {
+		return alumnosDao.findByCurso(curso)
+				.stream()
+				.map(a->alumnoConverter.alumnoToDto(a))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void eliminarAlumno(String nombre) {
+		alumnosDao.deleteByNombre(nombre);
+		
+	}
+
+	@Override
+	public List<AlumnoDto> todosAlumnos() {
+		return alumnosDao.findAll()
+				.stream()
+				.map(a->alumnoConverter.alumnoToDto(a))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void actualizarAlumno(AlumnoDto alumno) {
+		alumnosDao.save(alumnoConverter.dtoToAlumno(alumno));
+	}
+
+}
